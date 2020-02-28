cfa85a7ca0e856751cfd0f1854d2cab6fd694ed0
Unknown <t19960804@gmail.com>
2020-02-28 11:32:11 +0800

    New branch test
    as title

--- Podcasts/TestViewControlerl.swift
+++ Podcasts/TestViewControlerl.swift
@@ -11,7 +11,7 @@ import UIKit
 class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .blue
     }
 }
 
