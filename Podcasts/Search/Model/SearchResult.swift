a26ce5138a2e25b46cc3c1c4e58ed5ac8e94920c
Unknown <t19960804@gmail.com>
2020-09-06 13:54:42 +0800

    Add comment

--- Podcasts/Search/Model/SearchResult.swift
+++ Podcasts/Search/Model/SearchResult.swift
@@ -12,7 +12,8 @@ struct SearchResult: Decodable {
     let resultCount: Int
     let results: [Podcast]
 }
-
+//只要服從Codable protocol,就可以省略NSCoding / NSObject的繼承,並讓data decode to object,或object encode to data
+//https://medium.com/@wenchenx/swift-4-codable-%E8%AE%93%E5%BA%8F%E5%88%97%E5%8C%96%E8%AE%8A%E5%BE%97%E6%9B%B4%E7%B0%A1%E5%96%AE-73e55042f077
 struct Podcast: Codable {
     var trackName: String?
     var artistName: String?
