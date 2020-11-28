28e81e5f9bcb858355110e8defa21b28d0a9aa1b
Unknown <t19960804@gmail.com>
2020-11-28 14:16:32 +0800

    refactor code
    as title

--- Podcasts/EpisodesList/Controller/EpisodesListController.swift
+++ Podcasts/EpisodesList/Controller/EpisodesListController.swift
@@ -12,6 +12,9 @@ import FeedKit
 class EpisodesListController: UITableViewController {
     var podcast: Podcast! {
         didSet {
+            viewModel.isSearchingObserver = { [weak self] isSearching in
+                self?.searchingView.isHidden = !isSearching
+            }
             navigationItem.title = podcast.trackName
             guard let url = podcast.feedUrl else {
                 print("Error - feedUrl is nil")
@@ -83,7 +86,7 @@ class EpisodesListController: UITableViewController {
     }
     fileprivate func parseXMLFromURL(with url: String){
         guard let feedURL = URL(string: url) else { return }
-        searchingView.isHidden = false
+        viewModel.isSearching = true
         NetworkService.sharedInstance.fetchEpisodes(url: feedURL) { (result) in
             switch result {
             case .failure(let error):
@@ -99,7 +102,7 @@ class EpisodesListController: UITableViewController {
             }
             
             DispatchQueue.main.async { [self] in //Swift5.3改動 > 顯性的表明capture後,不用在block中隱性的加上self.xxx表明capture
-                searchingView.isHidden = true
+                viewModel.isSearching = false
                 tableView.reloadData()
             }
         }
@@ -162,10 +165,7 @@ class EpisodesListController: UITableViewController {
         return label
     }
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        let isSearching = searchingView.isHidden == false
-        if isSearching == false && episodes.isEmpty { //Searching完且沒有任何結果
-            return 200
-        }
-        return 0
+        viewModel.calculateFooterHeight()
+        return CGFloat(viewModel.footerHeight)
     }
 }
