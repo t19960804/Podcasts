b19f0756445d48c6b8b2ef0b88d8af0333931675
Unknown <t19960804@gmail.com>
2020-11-28 16:48:18 +0800

    Create player view Model
    as title

---
+++ Podcasts/Player/ViewModel/EpisodePlayerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EpisodePlayerViewModel.swift
+//  Podcasts
+//
+//  Created by t19960804 on 11/28/20.
+//  Copyright © 2020 t19960804. All rights reserved.
+//
+
+import Foundation
+
+class EpisodePlayerViewModel {
+    
+}
