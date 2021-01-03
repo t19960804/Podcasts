44facbf924f626a1bd7cd1951b3bc7331040d99a
Unknown <t19960804@gmail.com>
2021-01-03 15:24:27 +0800

    Add test case
    as title

--- PodcastsTests/EpisodePlayerViewTests.swift
+++ PodcastsTests/EpisodePlayerViewTests.swift
@@ -32,6 +32,30 @@ class EpisodePlayerViewTests: XCTestCase {
         let curEp = viewModel.currentEpisode
         XCTAssertEqual(curEp?.title, "ep3")
     }
+    func testPlayNextEpisode_NilEp(){
+        let result = viewModel.playNextEpisode(currentEpisode: nil)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
+    }
+    func testPlayNextEpisode_EmptyList(){
+        let ep1 = EpisodeCellViewModel(title: "ep1", author: "Tony")
+        viewModel.episodesList = []
+        let result = viewModel.playNextEpisode(currentEpisode: ep1)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
+    }
+    func testPlayNextEpisode_CantGetIndex(){
+        let ep1 = EpisodeCellViewModel(title: "ep1", author: "Tony")
+        let ep2 = EpisodeCellViewModel(title: "ep2", author: "Tony")
+        let ep3 = EpisodeCellViewModel(title: "ep3", author: "Tony")
+        viewModel.episodesList = [ep1,ep2]
+        let result = viewModel.playNextEpisode(currentEpisode: ep3)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
+    }
     func testPlayPreviousEpisode(){
         let ep1 = EpisodeCellViewModel(title: "ep1", author: "Tony")
         let ep2 = EpisodeCellViewModel(title: "ep2", author: "Tony")
@@ -40,6 +64,30 @@ class EpisodePlayerViewTests: XCTestCase {
         let _ = viewModel.playPreviousEpisode(currentEpisode: ep2)
         let curEp = viewModel.currentEpisode
         XCTAssertEqual(curEp?.title, "ep1")
+    }
+    func testPlayPreviousEpisode_NilEp(){
+        let result = viewModel.playPreviousEpisode(currentEpisode: nil)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
+    }
+    func testPlayPreviousEpisode_EmptyList(){
+        let ep1 = EpisodeCellViewModel(title: "ep1", author: "Tony")
+        viewModel.episodesList = []
+        let result = viewModel.playPreviousEpisode(currentEpisode: ep1)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
+    }
+    func testPlayPreviousEpisode_CantGetIndex(){
+        let ep1 = EpisodeCellViewModel(title: "ep1", author: "Tony")
+        let ep2 = EpisodeCellViewModel(title: "ep2", author: "Tony")
+        let ep3 = EpisodeCellViewModel(title: "ep3", author: "Tony")
+        viewModel.episodesList = [ep1,ep2]
+        let result = viewModel.playPreviousEpisode(currentEpisode: ep3)
+        let curEp = viewModel.currentEpisode
+        XCTAssertEqual(curEp?.title, nil)
+        XCTAssertEqual(result, false)
     }
     func testUpdateTimeSliderValue(){
         let curTime = CMTime(value: 5, timescale: 1000)
@@ -63,5 +111,19 @@ class EpisodePlayerViewTests: XCTestCase {
         viewModel.calculateSeekTime_RewindAndFastforward(currentTime: curTime, tag: 2)
         let targetTime2 = CMTime(value: 45, timescale: 1)
         XCTAssertEqual(viewModel.seekTime, targetTime2)
+    }
+    func testNeedToPausePlayerObserver_NeedtoPlay(){
+        viewModel.needToPausePlayerObserver = { (needToPausePlayer,image) in
+            XCTAssertEqual(needToPausePlayer, false)
+            XCTAssertEqual(image, UIImage(named: "pause"))
+        }
+        viewModel.needToPausePlayer = false
+    }
+    func testNeedToPausePlayerObserver_NeedtoPause(){
+        viewModel.needToPausePlayerObserver = { (needToPausePlayer,image) in
+            XCTAssertEqual(needToPausePlayer, true)
+            XCTAssertEqual(image, UIImage(named: "play"))
+        }
+        viewModel.needToPausePlayer = true
     }
 }
